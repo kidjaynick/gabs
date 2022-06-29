b060aa46157602e10cb31119f23bec76197c3868
unknown <gakidima001@gmail.com>
2022-06-29 07:39:34 -0700

    conditional

---
+++ hello/first.py
@@ -0,0 +1,18 @@
+# def remove_chars(word, n):
+#     x = word[n:]
+#     return X
+
+
+# print(remove_chars("mmmmad", 2))
+
+num = 37
+
+if num > 32:
+    print(5)
+
+elif num > 23:
+    print(10)
+
+else:
+    print(4)    
+
