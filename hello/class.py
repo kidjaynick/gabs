81640bccac05a63397337ebd671c61a5be4fe272
unknown <gakidima001@gmail.com>
2022-06-29 03:31:55 -0700

    first

---
+++ hello/class.py
@@ -0,0 +1,65 @@
+# kin = "he is a great guy and he is nice"
+# lin = "this,is a,string"
+
+# search_for = "is" 
+# total = string.count(search_for)
+
+# print(f"{total} result(s) found")
+#print(lin.replace(search_for, search_for.upper()))
+
+#print(lin.replace(" ", "-"))
+
+# print(lin.split(","))
+
+# str_list = ["this", "is", "a", "string"]
+
+# joined = " ".join(str_list)
+
+# print(joined)
+
+# def add_numbers(a:int, b:int):
+#     """This function adds numbers.
+    
+#     Args:
+#         a (int): this is the frist number to be added
+#         b (int): this is the second number to be added
+
+#     Returns:
+#         a+b (int): this is the addition of a and b
+#     """
+#     return a+b
+
+# b = add_numbers(688,958)   
+# print(b)
+
+def simple_interest(p:int, r:int, t:int):
+    """this function multiples numbers.
+
+    Arges:
+        p (int): this is the principle
+        r (int): this is the rate
+        t (int): this is the time
+
+    Returns:
+        p*r*t(int):this is the multiplication of p, r and t
+    """
+    return p*(r/100)*t
+
+b = simple_interest(53535,8,5)
+print(b)
+
+def perimeter_of_a_circle(p:int, r:int):
+    """this function multiples numbers.
+
+#     Arges:
+#         p = 3.142
+#         p(int): this is pie
+#         r(int): this is the radius
+    
+#     Returns:
+#         2*p*r(int): this is the perimeter of a circle
+#     """
+#     return 2*p*r
+# b = perimeter_of_a_circle(9,7)
+# print(b)
+
---
+++ practical/mama.py
@@ -0,0 +1,53 @@
+from ast import Compare
+from operator import index
+
+
+#a = "gabriel"
+
+# print(a[:])
+
+# greeting = "Hello, morning"
+
+# new_greeting = "j"+ greeting[1:]
+# print(new_greeting)
+
+#print(a.lower())
+
+#print(a.count("r"))
+
+#print(in_both("apples", "oranges"))
+ 
+s2 = "abce"
+
+#print(s1[1:10:2])
+
+b = 5685678
+#print(s1.capitalize())
+
+#print(s1.count("lm")) 
+
+#print(s1.endswith(""))
+#print(s1.startswith("b"))
+
+#print(s1.find("o"))
+#print(s1.rfind("lm"))
+
+#print("agh".isnumeric())
+
+#print(s1.center(90, "/"))
+
+s1 = """the greater you are the harder you stand
+ but that was when the world was in order"""
+
+print(s1.lstrip())
+print(s1.partition("the"))
+# print(s1.rpartition("lm"))
+
+print(s1.split("er"))
+print(s1.splitlines())
+
+
+
+
+
+
