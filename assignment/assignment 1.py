48b43bc357000e27ccfe929f1e8c9f2046ebdeab
unknown <gakidima001@gmail.com>
2022-07-03 03:59:54 -0700

    new

---
+++ assignment/assignment 1.py
@@ -0,0 +1,16 @@
+r = "gerry isn't a bad person"
+
+print(r.title())
+
+m =  "I am learning python"
+
+n = m.replace("I", "you")
+
+print(n)
+
+a = "i hope you had fun today in class"
+print(a.count("a"))
+
+p = "a**n + b**n = c**n"
+
+print("n =")
